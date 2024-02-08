960fa4f6bae9182cae8a6ccf4d12cb736acb9291
Adriel <adriel.fsantos@outlook.com>
2024-02-08 16:54:28 -0300

    feat: Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import clsx from 'clsx';
+import { ComponentPropsWithoutRef } from 'react';
+
+interface IProps extends Omit<ComponentPropsWithoutRef<'button'>, 'className'> {
+  fullWidth?: boolean;
+}
+
+export default function Button({ fullWidth, children, ...props }: IProps) {
+  return (
+    <button
+      className={clsx({
+        'bg-pink-500 rounded px-4 py-2 text-white': true,
+        'w-full': fullWidth,
+      })}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}

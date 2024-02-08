6fc6a9faf5ff208861fda95a3bbeb3012d87d524
Adriel <adriel.fsantos@outlook.com>
2024-02-08 19:07:20 -0300

    feat: Add route for Create page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { RouteObject, createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
+import Create from '../pages/Create';
 
 const routes: RouteObject[] = [
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/create',
+    element: <Create />,
   },
 ];
 

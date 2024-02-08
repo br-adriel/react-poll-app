870094c72994b56bd8e4d7f4dbd50e1525a58a47
Adriel <adriel.fsantos@outlook.com>
2024-02-08 16:24:45 -0300

    feat: Add Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,15 @@
+import CardLink from '../components/CardLink';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className='bg-pink-100'>
+      <div className='container mx-auto min-h-dvh p-6 flex justify-center items-center flex-col gap-5'>
+        <h1 className='text-7xl italic font-bold'>VotAí!</h1>
+        <div className='flex gap-3 flex-col w-[480px] max-w-full'>
+          <CardLink to='/create'>Criar uma enquete</CardLink>
+          <CardLink to='/vote'>Votar em uma enquete</CardLink>
+        </div>
+      </div>
+    </main>
+  );
 }

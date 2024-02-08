f72aca92b9180283916ed14713d774a2d4f5daa3
Adriel <adriel.fsantos@outlook.com>
2024-02-08 19:02:14 -0300

    feat: Update outline look on Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export default function Button({ fullWidth, children, ...props }: IProps) {
   return (
     <button
       className={clsx({
-        'bg-pink-500 rounded px-4 py-2 text-white hover:bg-pink-600 transition-colors':
+        'bg-pink-500 rounded px-4 py-2 text-white hover:bg-pink-600 transition-colors focus:outline-pink-600 focus:outline-offset-2':
           true,
         'w-full': fullWidth,
       })}

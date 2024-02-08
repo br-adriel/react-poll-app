8083e20f07c12c73ca7bd0aed47f05c13cb4624e
Adriel <adriel.fsantos@outlook.com>
2024-02-08 19:33:09 -0300

    feat: Add custom className on Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,11 @@
-import clsx from 'clsx';
 import { ComponentPropsWithoutRef } from 'react';
 
-interface IProps extends Omit<ComponentPropsWithoutRef<'button'>, 'className'> {
-  fullWidth?: boolean;
-}
+interface IProps extends ComponentPropsWithoutRef<'button'> {}
 
-export default function Button({ fullWidth, children, ...props }: IProps) {
+export default function Button({ children, className, ...props }: IProps) {
   return (
     <button
-      className={clsx({
-        'bg-pink-500 rounded px-4 py-2 text-white hover:bg-pink-600 transition-colors focus:outline-pink-600 focus:outline-offset-2':
-          true,
-        'w-full': fullWidth,
-      })}
+      className={`bg-pink-500 rounded px-4 py-2 text-white hover:bg-pink-600 transition-colors focus:outline-pink-600 focus:outline-offset-2 ${className}`}
       {...props}
     >
       {children}

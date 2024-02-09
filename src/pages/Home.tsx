5120685adc3d821d06f330dbdaa647801f25c250
Adriel <adriel.fsantos@outlook.com>
2024-02-09 18:58:50 -0300

    refactor: Remove repeated code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 import CardLink from '../components/CardLink';
-import Container from '../components/Container';
-import LogoHomeLink from '../components/LogoHomeLink';
 
 export default function Home() {
   useEffect(() => {
@@ -9,14 +7,9 @@ export default function Home() {
   }, []);
 
   return (
-    <main className='bg-pink-100'>
-      <Container>
-        <LogoHomeLink />
-        <div className='flex gap-3 flex-col w-[480px] max-w-full'>
-          <CardLink to='/create'>Criar uma enquete</CardLink>
-          <CardLink to='/vote'>Votar em uma enquete</CardLink>
-        </div>
-      </Container>
-    </main>
+    <div className='flex gap-3 flex-col w-[480px] max-w-full'>
+      <CardLink to='/create'>Criar uma enquete</CardLink>
+      <CardLink to='/vote'>Votar em uma enquete</CardLink>
+    </div>
   );
 }

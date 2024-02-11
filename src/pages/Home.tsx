9b24f2306ac3947334c4537cb4ffc4a4cfc1f79b
Adriel <adriel.fsantos@outlook.com>
2024-02-11 14:13:42 -0300

    feat: Add icons for Home buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { BiCheckSquare, BiPlus } from 'react-icons/bi';
 import CardLink from '../components/CardLink';
 
 export default function Home() {
@@ -8,8 +9,18 @@ export default function Home() {
 
   return (
     <div className='flex gap-3 flex-col w-[480px] max-w-full'>
-      <CardLink to='/create'>Criar uma enquete</CardLink>
-      <CardLink to='/poll'>Votar em uma enquete</CardLink>
+      <CardLink to='/create'>
+        <div className='flex gap-2 items-center'>
+          <BiPlus className='text-pink-600 text-2xl' />
+          Criar uma enquete
+        </div>
+      </CardLink>
+      <CardLink to='/poll'>
+        <div className='flex gap-2 items-center'>
+          <BiCheckSquare className='text-pink-600 text-2xl' />
+          Votar em uma enquete
+        </div>
+      </CardLink>
     </div>
   );
 }

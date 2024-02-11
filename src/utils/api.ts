753b2cd6c9e3cbe9c9d219fccc4229e74d2a83c8
Adriel <adriel.fsantos@outlook.com>
2024-02-10 21:47:14 -0300

    feat: Update handleError to deal with loading toast

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,18 +1,30 @@
-import { toast } from 'react-toastify';
+import { Id, toast } from 'react-toastify';
 
 interface handleErrorArgs {
   error: any;
   defaultMessage?: string;
+  toastId?: Id;
 }
 
 export function handleError({
   error,
   defaultMessage = 'Erro interno, tente novamente mais tarde!',
+  toastId,
 }: handleErrorArgs) {
-  if (error.response?.status == 500) {
-    toast.error(defaultMessage);
-    return;
+  let message = defaultMessage;
+
+  if (error.response?.status < 500 && error.response?.data.message) {
+    message = error.response.data.message;
   }
 
-  toast.error(error.response?.data.message || defaultMessage);
+  if (toastId) {
+    toast.update(toastId, {
+      render: defaultMessage,
+      type: 'error',
+      isLoading: false,
+      autoClose: 5000,
+    });
+    return;
+  }
+  toast.error(defaultMessage);
 }

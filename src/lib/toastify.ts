9722cd60620898cbd69c5dd5f72b5b0847f9301c
Adriel <adriel.fsantos@outlook.com>
2024-02-11 13:21:29 -0300

    feat: Add toastify options object

---
+++ src/lib/toastify.ts
@@ -0,0 +1,10 @@
+import { ToastOptions } from 'react-toastify';
+
+export const options: ToastOptions = {
+  autoClose: 3000,
+  closeButton: true,
+  closeOnClick: true,
+  draggable: 'touch',
+  draggableDirection: 'x',
+  position: 'bottom-center',
+};
